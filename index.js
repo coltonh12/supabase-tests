2ea787d3aa3b09e0062ae4069d1173b0dcd26ace
coltonh12 <145588758+coltonh12@users.noreply.github.com>
2024-03-14 21:14:26 +0000

    k

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://vmhdyikknzawqfvslqdl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGR5aWtrbnphd3FmdnNscWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDMxMzEsImV4cCI6MjAyNjAxOTEzMX0.KY21eyLCqGdjd6U7zMtFWHr5mGS0ogAHld1TMK1rLWE'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+let { data: Books, error } = await supabase
+  .from('Books')
+  .select('*')
